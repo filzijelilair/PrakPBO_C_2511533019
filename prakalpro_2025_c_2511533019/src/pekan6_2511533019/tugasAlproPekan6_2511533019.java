f31e2b902ed9a20081dd41125edf4db4a482c0a9
Filzi Jelila <Filzi Jelila@FILZI>
2025-11-09 12:32:30 +0700

    tugas praktikum Alpro pekan6 (Filzi Jelila)

--- prakalpro_2025_c_2511533019/src/pekan6_2511533019/tugasAlproPekan6_2511533019.java
+++ prakalpro_2025_c_2511533019/src/pekan6_2511533019/tugasAlproPekan6_2511533019.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class tugasAlproPekan6_2511533019 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		        
 		Scanner input = new Scanner(System.in);
 		Random rand = new Random();
 		int tries = 0;
@@ -16,32 +16,35 @@ public class tugasAlproPekan6_2511533019 {
 
 		while (running) {
 		tries++;
-			int dadu1 = rand.nextInt(6) + 1;
-	     		int dadu2 = rand.nextInt(6) + 1;
-			      sum = dadu1 + dadu2;
-		          System.out.println(dadu1+" + " +dadu2+ " = "+sum);
+		      int dadu1 = rand.nextInt(6) + 1;
+		      int dadu2 = rand.nextInt(6) + 1;
+		            sum = dadu1 + dadu2;
+		            System.out.println(dadu1+" + " +dadu2+ " = "+sum);
 
-		if (sum == 7) {
-		    System.out.println("Tebakan Anda Benar");
-		    System.out.println("Anda menang setelah " +tries+ " percobaan!");
-		    menang = true;
-		    running = false;
-		} else {
-		    System.out.println("Tebakan Anda Salah");
-		    System.out.print("Apakah mau lempar dadu (ya/tidak?)");
-			  String jawab = input.nextLine();
+		      if (sum == 7) {
+		            System.out.println("Tebakan Anda Benar");
+		            System.out.println("Anda menang setelah " +tries+ " percobaan!");
+		            menang = true;
+		            running = false;
+		      } else {
+		    	  	System.out.println("Tebakan Anda Salah");
+		            System.out.print("Apakah mau lempar dadu (ya/tidak?)");
+						String jawab = input.nextLine();
 			 
-		if (!jawab.equalsIgnoreCase("ya")) {
-		   running = false;
-		} else if (tries >= 5) {
-		   running = false;
-	  	}
-	  }
-	}
-		if (!menang) {
-		   System.out.println("Anda gagal menang");
+			  if (!jawab.equalsIgnoreCase("ya")) {
+				  running = false;
+			  } else if (tries >= 5) {
+				  running = false;
+			  }
+		  }
 		}
+			  if (!menang) {
+		         System.out.println("Anda gagal menang");
+		      }
 			  
-		input.close();
+		      input.close();
 		    }
 		}
+	
+
+
